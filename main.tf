0d081d38228042321d7ba6c3ad5d5ba8c795b9be
Roman Vyakhirev <roman.vyakhirev@gmail.com>
2023-10-03 12:50:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+  # access key have to be set in env variables
 }
 
 resource "aws_iam_account_password_policy" "strict" {
